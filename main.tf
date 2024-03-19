76ac81c34245e65b5375e15b1e46c7504bffbd55
unknown <kumaradarsh921@gmail.com>
2024-03-19 19:00:24 +0530

    Git Auto push

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 module "vpc" {
     source = "git::https://github.com/Adarsh-Pixel/tf-module-vpc.git?ref=main"
 }
+
+# We cannot parameterize anything that is added in the source
+
+# To limit that, we can use a tool called as Terrafile. All it does is clones the remote code from the specified branch and make it locally available. 
